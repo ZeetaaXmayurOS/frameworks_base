eec04f267c8e19c3af78a6070cb5a91328711a50
George Zacharia <george.zcharia@gmail.com>
2022-03-07 19:44:14 +0530

    fwb: Silence some AOSP logspam
    Change-Id: I967c9bec0fde47fc1da24f498cbabde2a7a5aa9f
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -1698,7 +1698,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
                 && !mDozing
                 && !ONLY_CORE_APPS;
         mNotificationPanelViewController.setQsExpansionEnabledPolicy(expandEnabled);
-        Log.d(TAG, "updateQsExpansionEnabled - QS Expand enabled: " + expandEnabled);
+        if (DEBUG) Log.d(TAG, "updateQsExpansionEnabled - QS Expand enabled: " + expandEnabled);
     }
 
     @Override
